40b0b5b7c0483fe818fdfe9aa812d53060344a52
Roman Nikitin <n75rom@gmail.com>
2021-05-22 22:04:04 +0900

    Fixed a bug in 'menuStatDateLastRec.sh'
    Fixed a bug with displaying the file name

--- src/functions/menu/menuStatDateLastRec.sh
+++ src/functions/menu/menuStatDateLastRec.sh
@@ -3,19 +3,19 @@
 source $HOME/.diaryNXBC/src/functions/walkFolderShowFiles.sh
 
 function compareStrDate () {
-	echo -e "$1"'\n'"$2" | sort -r | head -n 1
+  echo -e "$1"'\n'"$2" | sort -r | head -n 1
 }
 
 maxDate=0
 function walkFolder() {
-	for fileName in "$1"/*; do
-		if [ -d "${fileName}" ]; then
-			walkFolder "${fileName}"
-		elif [ -f "${fileName}" ]; then
-			str=$(echo "${fileName}" | grep --only-matching --perl-regexp "\d*-\d*-\d*_\d*-\d*")
-			maxDate=$(compareStrDate ${maxDate} ${str})
-		fi
-	done
+  for fileName in "$1"/*; do
+    if [ -d "${fileName}" ]; then
+      walkFolder "${fileName}"
+    elif [ -f "${fileName}" ]; then
+      str=$(echo "${fileName}" | grep --only-matching --perl-regexp "\d*-\d*-\d*_\d*-\d*")
+      maxDate=$(compareStrDate ${maxDate} ${str})
+    fi
+  done
 }
 
 file="${DIARY_DIR}/.tempList"
@@ -23,10 +23,10 @@ walkFolderShowFiles "${DIARY_DIR}" > "${file}"
 isExist=$?
 
 if [ $(echo ${isExist}) -eq 1 ]; then
-	walkFolder "${DIARY_DIR}"
-	echo -n "The last record was created: ${maxDate} \"("
-	echo "$(grep --only-matching --perl-regexp "/home.*${maxDate}\.md" ${file}))\""
+  walkFolder "${DIARY_DIR}"
+  echo -n "The last record was created: ${maxDate} \"("
+  echo "$(grep --only-matching --perl-regexp --recursive "${DIARY_DIR}/.*${maxDate}\.md" ${file}))\""
 else
-	echo "Records not found"
+  echo "Records not found"
 fi
 rm "${file}"
