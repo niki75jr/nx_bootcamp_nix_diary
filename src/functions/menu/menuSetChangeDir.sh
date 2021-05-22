594ffc77045275d6bd92d40230f1dae2f24b9d17
Roman Nikitin <n75rom@gmail.com>
2021-05-22 13:31:03 +0000

    Edited 'menuSetChangeDir.sh'
    Added a restriction for version 'Alpine' when changing the diary-directory

--- src/functions/menu/menuSetChangeDir.sh
+++ src/functions/menu/menuSetChangeDir.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-source $HOME/.diaryNXBC/src/functions/replace.sh
+if [ "${UBUNTU}" != 'false' ]; then
+  source $HOME/.diaryNXBC/src/functions/replace.sh
 
-echo "---Default directory for this diary is \"${DIARY_DIR}\""
-echo -n "new default: "
-read -p "$(echo -n "->: ${HOME}/")"
-echo -n "\"${DIARY_DIR}\"--->"
-replace "DIARY_DIR" "${HOME}/${REPLY}"
-replace "DIARY_BACKUPS_DIR" "${HOME}/${REPLY}/.backups"
-mkdir --parents "${HOME}/${REPLY}"
-echo "\"${HOME}/${REPLY}\""
+  echo "---Default directory for this diary is \"${DIARY_DIR}\""
+  echo -n "new default: "
+  read -p "$(echo -n "->: ${HOME}/")"
+  echo -n "\"${DIARY_DIR}\"--->"
+  replace "DIARY_DIR" "${HOME}/${REPLY}"
+  replace "DIARY_BACKUPS_DIR" "${HOME}/${REPLY}/.backups"
+  mkdir --parents "${HOME}/${REPLY}"
+  echo "\"${HOME}/${REPLY}\""
+else
+  echo 'Sorry, but this function is not available for this version of Linux.'
+fi
